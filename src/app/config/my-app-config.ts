c89aed21d637dbfe0e062f6aeba994b7e94fd6d8
OuniAbir <ouniabir118@gmail.com>
2021-01-04 09:22:48 +0100

    Security - Login/logout - set up config for OpenId Connect

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export export {
+oicd : {
+  clientId :'0oa3azani1KQPKTTJ5d6' ,
+  issuer : 'dev-1302852.okta.com',
+  redirectUri : 'http://localhost:8080/4200/login/callback' ,
+  scopes : ,
+}
+
+}
