ebcb0f9f7fac6fb2d22dde767f31c67105fa726b
OuniAbir <ouniabir118@gmail.com>
2021-01-04 14:58:10 +0100

    springBoot backend is sending HATEOAS not JSON : update the checkout form service

--- src/app/services/checkout-form.service.ts
+++ src/app/services/checkout-form.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Country } from '../common/country';
+import { map } from 'rxjs/operators';
+import { State } from '../common/state';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutFormService {
 
-  private baseUrl = "http://localhost:8080/spring-mvc-ecommerce/api/";
+  private baseUrl = "http://localhost:8080/spring-mvc-ecommerce/api";
   constructor( private httpClient: HttpClient) { }
   getCountries():Observable<any>{
-    return this.httpClient.get(`${this.baseUrl}countries`)
+    console.log(`${this.baseUrl}countries`);
+    return this.httpClient.get<GetResponseCountries>(`${this.baseUrl}/countries`)
+    .pipe(map(response => response._embedded.countries));
   }
   getStatesByCountryCode(code : string): Observable<any>{
-    return this.httpClient.get(`${this.baseUrl}states/search/findByCountryCode?code=${code}`);
+    console.log(`${this.baseUrl}/states/search/findByCountryCode?code=${code}`);
+    return this.httpClient.get<GetResponseStates>(`${this.baseUrl}/states/search/findByCountryCode?code=${code}`)
+    .pipe(map(response => response._embedded.states));
+  }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
   }
 }
