dd198dc3ca0a6857cc8a667fab9744ecc30ca3e2
OuniAbir <ouniabir118@gmail.com>
2021-01-04 09:41:52 +0100

    Security - Login/logout - install okta SDK dependencies

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,9 +1,10 @@
-export export {
+export default {
+  //Open ID Connec config
 oicd : {
   clientId :'0oa3azani1KQPKTTJ5d6' ,
-  issuer : 'dev-1302852.okta.com',
-  redirectUri : 'http://localhost:8080/4200/login/callback' ,
-  scopes : ,
+  issuer : 'http://dev-1302852.okta.com/oauth2/default', // distributeur of tokens
+  redirectUri : 'http://localhost:4200/login/callback' ,
+  scopes : ['openid', 'profile','email' ], //scopes provides access to info abt the user
 }
 
 }
